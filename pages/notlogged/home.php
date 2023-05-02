75b27c63e14e4711b3c9c4205f3ae68ce70e65ac
Sena <117431055+SenaNurzhan@users.noreply.github.com>
2023-05-02 23:04:17 +0600

    Add files via upload

--- pages/notlogged/home.php
+++ pages/notlogged/home.php
@@ -1,1 +1,22 @@
+<section id="login">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 center">
+                <div class="form-wrap">
+                <h1 style = "color:#009900">Log in with your Login</h1>
+                    <form role="form" action="?act=login" method="post" id="login-form" autocomplete="off">
+                        <div class="form-group">
+                            <input type="text" name = "login" id="email" placeholder="Login" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" name="password" id="password" placeholder="Password" class="form-control">
+                        </div>
+                        <input type="submit" id="btn-login" class="btn btn-custom btn-lg btn-block" value="Log in" style = "background-color:#008000">
+                        <a href="?act=regis"><button  type="button" class="btn btn-custom btn-lg btn-block" style = "background-color:#ff7733">Registration</button></a>
+                    </form>
 
+                </div>
+            </div> <!-- /.col-xs-12 -->
+        </div> <!-- /.row -->
+    </div> <!-- /.container -->
+</section>
---
+++ pages/notlogged/registration.php
@@ -0,0 +1,59 @@
+<div class="container">
+        <div class="row centered-form">
+        <div class="col-xs-12 col-sm-8 col-md-4 col-sm-offset-2 col-md-offset-4">
+        	<div class="panel panel-default" style = "height:400px">
+        		<div class="panel-heading">
+			    	<h3 class="panel-title" style = "color:#008000" >Please sign up</h3>
+			 	</div>
+			 		<div class="panel-body">
+			    		<form role="form" action="?act=reg" method = "post">
+			    			<div class="row">
+			    				<div class="col-xs-6 col-sm-6 col-md-6">
+			    					<div class="form-group">
+			                <input type="text" name="name" id="first_name" class="form-control input-sm" placeholder="Name">
+			    					</div>
+			    				</div>
+			    				<div class="col-xs-6 col-sm-6 col-md-6">
+			    					<div class="form-group">
+			    						<input type="text" name="surname" id="last_name" class="form-control input-sm" placeholder="Surname">
+			    					</div>
+			    				</div>
+			    			</div>
+
+			    			<div class = "row">
+			    				<div class="col-xs-6 col-sm-6 col-md-12">
+									<div class="form-group">
+										<input type="text" name="age" id="email" class="form-control input-sm" style = "font-size:15px" placeholder = "Age">
+									</div>
+								</div>
+							</div>	
+
+			    			<div class="form-group">
+			    				<input type="text" name="login" id="email" class="form-control input-sm" placeholder = "Login">
+			    			</div>
+
+			    			<div class="row">
+			    				<div class="col-xs-6 col-sm-6 col-md-6">
+			    					<div class="form-group">
+			    						<input type="password" name="password" id="password" class="form-control input-sm" placeholder="Password">
+			    					</div>
+			    				</div>
+			    				<div class="col-xs-6 col-sm-6 col-md-6">
+			    					<div class="form-group">
+			    						<input type="password" name="con-password" id="password_confirmation" class="form-control input-sm" placeholder="Confirm Password">
+			    					</div>
+			    				</div>
+			    			</div>
+			    			<hr>
+
+			    			<input type="submit" value="Register" class="btn btn-info btn-block" style="background-color:#006600"><br>
+			    		
+			    		</form>
+			    		<form>
+			    			<input action = "?act=back" type="submit" value="Go Back" class="btn btn-info btn-block"style="background-color:#ff3300">
+			    		</form>
+			    	</div>
+	    		</div>
+    		</div>
+    	</div>
+    </div>
