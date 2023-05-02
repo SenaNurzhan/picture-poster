8650c7b19972abb280f0e5ae9db3a66153c84708
Sena <117431055+SenaNurzhan@users.noreply.github.com>
2023-05-02 23:01:55 +0600

    Add files via upload

---
+++ templates/footer.php
@@ -0,0 +1,9 @@
+<footer>
+   <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <p style="font-size:13px" >Copyright &copy; Developed by <strong>SENA.KZ</strong> 2016</p>
+            </div>
+        </div>
+    </div>
+</footer>
